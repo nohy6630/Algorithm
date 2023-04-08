ec2bcb97f5e9c3a5d766a482f8ed5543d7b7039c
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-04-09 01:00:17 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 32 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
@@ -1,76 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+#define MAX 10000
 
 using namespace std;
-typedef struct Node {
-	int left;
-	int right;
-} Node;
-typedef struct Width {
-	int start;
-	int end;
-} Width;
 
-Node graph[10001];
-Width widths[10001];
-int col = 0;
-int N;
-
-void init_widths(int n, int depth)
+struct Node
 {
-	if (graph[n].left != -1)
-		init_widths(graph[n].left, depth + 1);
-	col++;
-	if (!widths[depth].start)
-	{
-		widths[depth].start=col;
-		widths[depth].end = col;
-	}
-	else
-		widths[depth].end = col;
-	if (graph[n].right != -1)
-		init_widths(graph[n].right, depth + 1);
-}
+	int left, right;	
+};
 
-int get_root()
+Node tree[MAX + 1];
+int pos=0,left_node[MAX+1],right_node[MAX+1],N;
+
+void inorder(int node, int depth)
 {
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			if (graph[j].left == i || graph[j].right == i)
-				break;
-			if (j == N)
-				return i;
-		}
-	}
-	return -1;
+	if(tree[node].left!=-1)
+		inorder(tree[node].left,depth+1);
+	pos++;
+	left_node[depth] = min(left_node[depth], pos);
+	right_node[depth] = max(right_node[depth], pos);
+	if(tree[node].right!=-1)
+		inorder(tree[node].right,depth+1);
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	int max = -1, level = -1;
-	cin >> N;
-	for (int i = 0; i < N; i++)
+	cin>>N;
+	for(int i=0;i<N;i++)
 	{
-		int a, b, c;
-		cin >> a >> b >> c;
-		graph[a] = { b,c };
+		int a,b,c;
+		cin>>a>>b>>c;
+		tree[a]={b,c};
 	}
-
-	init_widths(get_root(), 1);
-	for (int i = 1; i <= N; i++)
+	for(int i=1;i<=N;i++)
+		left_node[i]=INT_MAX;
+	for(int i=1;i<=N;i++)
 	{
-		int w = widths[i].end - widths[i].start + 1;
-		if (w > max)
+		bool is_root=true;
+		for(int j=1;j<=N;j++)
 		{
-			level = i;
-			max = w;
+			if(tree[j].left==i||tree[j].right==i)
+			{
+				is_root=false;
+				break;
+			}
+		}
+		if(is_root)
+		{
+			inorder(i, 1);
+			break;			
 		}
 	}
-	cout << level << ' ' << max;
-
+	int l, mw=0;
+	for(int i=1;i<=N;i++)
+	{
+		int w=right_node[i]-left_node[i]+1;
+		if(w>mw)
+		{
+			mw=w;
+			l=i;
+		}
+	}
+	cout<<l<<' '<<mw;
 	return 0;
 }
