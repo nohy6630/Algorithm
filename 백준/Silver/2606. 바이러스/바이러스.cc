53d597a71a09129142b7b79bcd7da9d9eb97a744
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-05-07 20:31:23 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,22 +1,21 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+vector<int> G[101];
 int N,M;
 int cnt;
-vector<int> G[101];
 bool visited[101];
 
 void dfs(int n)
 {
+	cnt++;
 	visited[n]=true;
-	for(auto com : G[n])
+	for(int adj:G[n])
 	{
-		if(visited[com])
+		if(visited[adj])
 			continue;
-		cnt++;
-		dfs(com);
+		dfs(adj);
 	}
 }
 
@@ -25,7 +24,7 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cin>>N>>M;
-	for(int i=0;i<M;i++)
+	for(int i=1;i<=M;i++)
 	{
 		int a,b;
 		cin>>a>>b;
@@ -33,6 +32,5 @@ int main()
 		G[b].push_back(a);
 	}
 	dfs(1);
-	cout<<cnt;
-	return 0;
+	cout<<cnt-1;
 }
