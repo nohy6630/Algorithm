1f5c4592a3aff59634b141255d97844033776577
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-05-07 20:52:02 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,36 +1,36 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int N;
 int board[129][129];
 int white,blue;
 
-void dq(int y, int x, int n)
+void dq(int y,int x,int n)
 {
 	bool chk=false;
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n;j++)
 		{
-			if(board[y+i][x+j] != board[y][x])
-			{
+			if(board[y+i][x+j]!=board[y][x])
 				chk=true;
-				break;	
-			}
 		}
 	}
-	if(!chk)
+	if(chk)
+	{
+		dq(y,x,n/2);
+		dq(y,x+n/2,n/2);
+		dq(y+n/2,x,n/2);
+		dq(y+n/2,x+n/2,n/2);
+	}
+	else
 	{
 		if(board[y][x])
 			blue++;
 		else
 			white++;
-		return;
 	}
-	dq(y,x,n/2);
-	dq(y+n/2,x,n/2);
-	dq(y,x+n/2,n/2);
-	dq(y+n/2,x+n/2,n/2);
 }
 
 int main()
@@ -39,9 +39,10 @@ int main()
 	cin.tie(0);
 	cin>>N;
 	for(int i=1;i<=N;i++)
+	{
 		for(int j=1;j<=N;j++)
 			cin>>board[i][j];
+	}
 	dq(1,1,N);
-	cout<<white<<'\n'<<blue;
-	return 0;
+	cout<<white<<'\n'<<blue<<'\n';
 }
