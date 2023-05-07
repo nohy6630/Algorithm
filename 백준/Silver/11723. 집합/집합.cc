3604407a43690756ec915041802ab40e948fb028
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-05-07 21:14:04 +0900

    [Silver V] Title: 집합, Time: 632 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int M;
-set<int> s;
+int S;
 
 int main()
 {
@@ -12,43 +12,26 @@ int main()
 	cin>>M;
 	for(int i=0;i<M;i++)
 	{
-		string op;
-		int n;
-		cin>>op;
-		if(op!="all"&&op!="empty")
-			cin>>n;
-		if(op=="add")
-			s.insert(n);
-		else if(op=="remove")
+		string s;int x;
+		cin>>s;
+		if(s!="empty"&&s!="all")
+			cin>>x;
+		if(s=="add")
+			S|=(1<<x);
+		else if(s=="remove")
+			S&=~(1<<x);
+		else if(s=="check")
 		{
-			s.erase(n);
-		}
-		else if(op=="check")
-		{
-			
-			if(s.find(n)!=s.end())
-				cout<<1<<'\n';
+			if(S&(1<<x))
+				cout<<"1\n";
 			else
-				cout<<0<<'\n';
+				cout<<"0\n";
 		}
-		else if(op=="toggle")
-		{
-			if(s.find(n)!=s.end())
-				s.erase(n);
-			else
-				s.insert(n);
-		}
-		else if(op=="all")
-		{
-			for(int j=1;j<=20;j++)
-				s.insert(j);
-		}
+		else if(s=="toggle")
+			S^=(1<<x);
+		else if(s=="empty")
+			S=0;
 		else
-			s.clear();
+			S=(1<<21)-1;
 	}
-	return 0;
 }
-
-/*
-비트로 집합을 나타낼수 있기 때문에 비트마스킹으로 문제를 풀 수있다는데 우선 그냥 set으로 해결했다. 
-*/
