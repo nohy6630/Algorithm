04ab91abaccfe2e86f41b1b7970ab82fb2fb139e
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-05-12 00:48:09 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+using ll = long long;
+
+
+ll func(ll a, ll b, ll c) {
+	if (b == 1)
+		return a % c;
+
+	ll val = func(a, b / 2, c);
+	if (b % 2 == 0)
+		return val * val % c;
+	else
+		return val * val % c * a % c;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+	cout << func(a, b, c);
+
+	return 0;
+}
