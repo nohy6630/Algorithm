95597cb1f8a6e5af2d6e577778d191a0d8d3c4f9
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-05-21 22:55:16 +0900

    [Gold II] Title: 골목길, Time: 4 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Gold/1738. 골목길/골목길.cc
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int,int> pii;
+
+int N,M;
+vector<pii> G[101];
+vector<int> revG[101];
+long long dist[101];
+int trace[101];
+bool visited[101];
+bool fail;
+vector<int> S;
+
+void bellmanford()
+{
+	fill(dist,dist+N+1,LLONG_MIN);
+	dist[1]=0;
+	for(int i=1;i<=N;i++)
+	{
+		for(int u=1;u<=N;u++)
+		{
+			if(dist[u]==LLONG_MIN)
+				continue;
+			for(auto adj:G[u])
+			{
+				int w=adj.X,v=adj.Y;
+				if(dist[u]+w>dist[v])
+				{
+					dist[v]=dist[u]+w;
+					trace[v]=u;
+					if(i==N&&visited[v])
+						fail=true;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin>>N>>M;
+	for(int i=0;i<M;i++)
+	{
+		int u,v,w;
+		cin>>u>>v>>w;
+		G[u].push_back({w,v});
+		revG[v].push_back(u);
+	}
+	queue<int> q;
+	q.push(N);
+	visited[N] = true;
+	while (!q.empty()) {
+		int cidx = q.front();
+		q.pop();
+		for (int i = 0; i < revG[cidx].size(); i++) {
+			int next = revG[cidx][i];
+			if (!visited[next]) {
+				visited[next] = true;
+				q.push(next);
+			}
+		}
+	}
+	bellmanford();
+	if(fail)
+		cout<<"-1";
+	else
+	{
+		int n=N;
+		while(n!=1)
+		{
+			S.push_back(n);
+			n=trace[n];
+		}
+		S.push_back(1);
+		while(!S.empty())
+		{
+			cout<<S.back()<<" ";
+			S.pop_back();
+		}
+	}
+}
