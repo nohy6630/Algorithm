95e5fef7272b3edf17582869b589b5682c99ece7
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-06-04 17:58:42 +0900

    [Platinum II] Title: 주차장, Time: 8 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Platinum/1348. 주차장/주차장.cc
+++ 백준/Platinum/1348. 주차장/주차장.cc
@@ -1,119 +1,124 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <utility>
-#define x first
-#define y second
+#include <bits/stdc++.h>
+
 using namespace std;
+typedef pair<int, int> pii;
 
-using p = pair<int, int>;
+int N, M;
+char board[51][51];
+vector<pii> A, B;
+int dist[101][101], d[101][101], park[201], visited[201];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+vector<int> G[201];
 
-char arr[55][55];
-vector<p> car, park;
-
-int dist[111][111];
-int chk[111][111];
-const int di[] = { 0, 0, 1, -1 };
-const int dj[] = { 1, -1, 0, 0 };
-
-int n, m;
-
-void getDist(int st){
-	memset(chk, -1, sizeof chk);
-	queue<p> q; q.push(car[st]);
-	chk[car[st].x][car[st].y] = 0;
-	while (q.size()){
-		auto now = q.front(); q.pop();
-		int i = now.x;
-		int j = now.y;
-		for (int k = 0; k < 4; k++){
-			int ii = i + di[k];
-			int jj = j + dj[k];
-			if (ii < 1 || jj < 1 || ii > n || jj > m) continue;
-			if (chk[ii][jj] != -1) continue;
-			if (arr[ii][jj] == 'X') continue;
-			chk[ii][jj] = chk[i][j] + 1;
-			q.push({ ii, jj });
-		}
-	}
-	for (int i = 1; i < park.size(); i++){
-		dist[st][i] = chk[park[i].x][park[i].y];
-	}
+bool dfs(int v)
+{
+    visited[v] = true;
+    for (auto adj : G[v])
+    {
+        if (visited[adj])
+            continue;
+        visited[adj] = true;
+        if (park[adj] == -1 || dfs(park[adj]))
+        {
+            park[adj] = v;
+            return true;
+        }
+    }
+    return false;
 }
 
-vector<int> g[222];
-int bias = 100;
-
-void makeGraph(int m){
-	for (int i = 1; i < car.size(); i++){
-		for (int j = 1; j < park.size(); j++){
-			if (dist[i][j] != -1 && dist[i][j] <= m){
-				g[i].push_back(j + bias);
-			}
-		}
-	}
+bool isValid(int t)
+{
+    for (int i = 0; i < 201; i++)
+        G[i].clear();
+    for (int i = 1; i < A.size(); i++)
+    {
+        for (int j = 1; j < B.size(); j++)
+        {
+            if (dist[i][j] != -1 && dist[i][j] <= t)
+                G[i].push_back(j+100);
+        }
+    }
+    int cnt = 0;
+    memset(park, -1, sizeof(park));
+    for (int i = 1; i < A.size(); i++)
+    {
+        memset(visited, 0, sizeof(visited));
+        cnt += dfs(i);
+    }
+    return cnt == A.size() - 1;
 }
 
-int par[222];
-int vis[222];
-
-bool dfs(int v){
-	vis[v] = 1;
-	for (auto i : g[v]){
-		if (vis[i]) continue;
-		vis[i] = 1;
-		if (par[i] == -1 || dfs(par[i])){
-			par[i] = v; return 1;
-		}
-	}
-	return 0;
+void bfs(int s)
+{
+    memset(d, -1, sizeof(d));
+    queue<pii> Q;
+    Q.push(A[s]);
+    d[A[s].first][A[s].second] = 0;
+    while (!Q.empty())
+    {
+        int x = Q.front().first, y = Q.front().second;
+        Q.pop();
+        for (int k = 0; k < 4; k++)
+        {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if (nx < 1 || ny < 1 || nx > N || ny > M)
+                continue;
+            if (d[nx][ny] != -1)
+                continue;
+            if (board[nx][ny] == 'X')
+                continue;
+            d[nx][ny] = d[x][y] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    for (int i = 1; i < B.size(); i++)
+        dist[s][i] = d[B[i].first][B[i].second];
 }
 
-bool decision(int m){
-	for (int i = 0; i < 222; i++) g[i].clear();
-	makeGraph(m);
-	int cnt = 0;
-	memset(par, -1, sizeof par);
-	for (int i = 1; i < car.size(); i++){
-		memset(vis, 0, sizeof vis);
-		cnt += dfs(i);
-	}
-	return cnt == car.size() - 1;
-}
-
-int main(){
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	cin >> n >> m;
-	car.push_back({ -1, -1 });
-	park.push_back({ -1, -1 });
-	for (int i = 1; i <= n; i++){
-		for (int j = 1; j <= m; j++){
-			cin >> arr[i][j];
-			if (arr[i][j] == 'C') car.push_back({ i, j });
-			if (arr[i][j] == 'P') park.push_back({ i, j });
-		}
-	}
-	for (int i = 1; i < car.size(); i++) getDist(i);
-
-	if (car.size() == 1){
-		cout << 0; return 0;
-	}
-	if (car.size() > park.size()){
-		cout << -1; return 0;
-	}
-	int l = 0, r = 10000;
-	int ans = -1;
-	while (l <= r){
-		int m = l + r >> 1;
-		if (decision(m)){
-			ans = m;
-			r = m - 1;
-		}
-		else{
-			l = m + 1;
-		}
-	}
-	cout << ans;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    A.push_back({-1, -1});
+    B.push_back({-1, -1});
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= M; j++)
+        {
+            cin >> board[i][j];
+            if (board[i][j] == 'C')
+                A.push_back({i, j});
+            if (board[i][j] == 'P')
+                B.push_back({i, j});
+        }
+    }
+    if (A.size() > B.size())
+    {
+        cout << -1;
+        return 0;
+    }
+    if (A.size() == 1)
+    {
+        cout << 0;
+        return 0;
+    }
+    for (int i = 1; i < A.size(); i++)
+        bfs(i);
+    int ans = -1, l = 0, r = 10000;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        if (isValid(m))
+        {
+            ans = m;
+            r = m - 1;
+        }
+        else
+            l = m + 1;
+    }
+    cout << ans;
 }
