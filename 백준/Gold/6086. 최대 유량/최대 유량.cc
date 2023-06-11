823393f8da9571ebd0b97747134ed267d45589f5
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-06-11 21:45:00 +0900

    [Gold III] Title: 최대 유량, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/6086. 최대 유량/최대 유량.cc
+++ 백준/Gold/6086. 최대 유량/최대 유량.cc
@@ -1,79 +1,73 @@
-#include <bits/stdc++.h>
-#define X first
-#define Y second
-#define MAX 53
+#include<bits/stdc++.h>
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-int capacity[MAX][MAX];
-int flow[MAX][MAX];
+int c[53][53];
+int f[53][53];
 int N;
-vector<int> G[MAX];
+vector<int> G[53];
 
-int edmonskarp(int src, int dest)
+int edmonskarp(int s,int t)
 {
-    int maxflow = 0;
-    while (1)
-    {
-        int parent[MAX];
-        queue<int> Q;
-        fill(parent, parent + MAX, 0);
-        parent[src] = src;
-        Q.push(src);
-        while (!Q.empty() && !parent[dest])
-        {
-            int cur = Q.front();
-            Q.pop();
-            for (int i : G[cur])
-            {
-                if (capacity[cur][i] - flow[cur][i] > 0 && !parent[i])
-                {
-                    parent[i] = cur;
-                    Q.push(i);
-                }
-            }
-        }
-        if (!parent[dest])
-            break;
-        int curflow = INT_MAX;
-        for (int i = dest; i != src; i = parent[i])
-            curflow = min(capacity[parent[i]][i] - flow[parent[i]][i], curflow);
-        for (int i = dest; i != src; i = parent[i])
-        {
-            flow[parent[i]][i] += curflow;
-            flow[i][parent[i]] -= curflow;
-        }
-        maxflow += curflow;
-    }
-    return maxflow;
+	int ret=0;
+	while(1)
+	{
+		int p[53];
+		queue<int> Q;
+		memset(p,0,sizeof(p));
+		p[s]=s;
+		Q.push(s);
+		while(!Q.empty()&&!p[t])//bfs 순회하면서 parent배열 세팅 
+		{
+			int u=Q.front();
+			Q.pop();
+			for(int v:G[u])
+			{
+				if(c[u][v]-f[u][v]>0&&!p[v])
+				{
+					p[v]=u;
+					Q.push(v);
+				}
+			}
+		}
+		if(!p[t])//현재 flow network에서는 d에 도달하지 못하므로 break 
+			break;
+		int flow = INT_MAX;
+		for(int i=t;i!=s;i=p[i])
+			flow=min(flow,c[p[i]][i]-f[p[i]][i]);//순회한 bfs경로를 탐색하면서 흘릴수있는 최대 flow찾음 
+		for(int i=t;i!=s;i=p[i])
+		{
+			f[p[i]][i]+=flow; 
+			f[i][p[i]]-=flow;
+		}
+		ret+=flow;
+	}
+	return ret;
 }
 
 int ctoi(char c)
 {
-    if (isupper(c))
-        return c - 'A' + 1;
-    else
-        return c - 'a' + 27;
+	if(isupper(c))
+		return c-'A'+1;
+	else
+		return c-'a'+27;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        char u, v;
-        int w;
-        cin >> u >> v >> w;
-        u = ctoi(u);
-        v = ctoi(v);
-        capacity[u][v] += w;
-        capacity[v][u] += w;
-        G[u].push_back(v);
-        G[v].push_back(u);
-    }
-    cout << edmonskarp(1, 26);
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	cin>>N;
+	for(int i=0;i<N;i++)
+	{
+		char u, v;
+		int w;
+		cin>>u>>v>>w;
+		u=ctoi(u);
+		v=ctoi(v);
+		G[u].push_back(v);
+		G[v].push_back(u);
+		c[u][v]+=w; 
+		c[v][u]+=w;
+	}
+	cout<<edmonskarp(1,26);
 }
