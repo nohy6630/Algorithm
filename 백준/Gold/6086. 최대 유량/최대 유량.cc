83216abc8f85094b0ea6fa471483f83d3cfd6526
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-06-11 22:01:45 +0900

    [Gold III] Title: 최대 유량, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/6086. 최대 유량/최대 유량.cc
+++ 백준/Gold/6086. 최대 유량/최대 유량.cc
@@ -1,23 +1,44 @@
 #include<bits/stdc++.h>
+#define MAX 53
 
 using namespace std;
 
-int c[53][53];
-int f[53][53];
+int c[MAX][MAX];
+int f[MAX][MAX];
 int N;
-vector<int> G[53];
+vector<int> G[MAX];
+int p[MAX];
 
-int edmonskarp(int s,int t)
+int ctoi(char c)
 {
-	int ret=0;
+	if(isupper(c))
+		return c-'A'+1;
+	return c-'a'+27;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	cin>>N;
+	for(int i=0;i<N;i++)
+	{
+		char u,v;
+		int w;
+		cin>>u>>v>>w;
+		u=ctoi(u);
+		v=ctoi(v);
+		c[u][v]+=w;
+		c[v][u]+=w;
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+	int ans=0;
 	while(1)
 	{
-		int p[53];
 		queue<int> Q;
 		memset(p,0,sizeof(p));
-		p[s]=s;
-		Q.push(s);
-		while(!Q.empty()&&!p[t])//bfs 순회하면서 parent배열 세팅 
+		Q.push(1);
+		while(!Q.empty()&&!p[26])
 		{
 			int u=Q.front();
 			Q.pop();
@@ -30,44 +51,17 @@ int edmonskarp(int s,int t)
 				}
 			}
 		}
-		if(!p[t])//현재 flow network에서는 d에 도달하지 못하므로 break 
+		if(!p[26])
 			break;
-		int flow = INT_MAX;
-		for(int i=t;i!=s;i=p[i])
-			flow=min(flow,c[p[i]][i]-f[p[i]][i]);//순회한 bfs경로를 탐색하면서 흘릴수있는 최대 flow찾음 
-		for(int i=t;i!=s;i=p[i])
+		int flow=INT_MAX;
+		for(int i=26;i!=1;i=p[i])
+			flow=min(flow,c[p[i]][i]-f[p[i]][i]);
+		for(int i=26;i!=1;i=p[i])
 		{
-			f[p[i]][i]+=flow; 
+			f[p[i]][i]+=flow;
 			f[i][p[i]]-=flow;
 		}
-		ret+=flow;
+		ans+=flow;
 	}
-	return ret;
-}
-
-int ctoi(char c)
-{
-	if(isupper(c))
-		return c-'A'+1;
-	else
-		return c-'a'+27;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0);cin.tie(0);
-	cin>>N;
-	for(int i=0;i<N;i++)
-	{
-		char u, v;
-		int w;
-		cin>>u>>v>>w;
-		u=ctoi(u);
-		v=ctoi(v);
-		G[u].push_back(v);
-		G[v].push_back(u);
-		c[u][v]+=w; 
-		c[v][u]+=w;
-	}
-	cout<<edmonskarp(1,26);
+	cout<<ans;
 }
