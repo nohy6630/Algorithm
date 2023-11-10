0b31aff5a3ba480775ef7e9124d45864346ccf43
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2023-11-11 01:57:49 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    int answer, target;
+    int[] numbers;
+    
+    void dfs(int val, int idx){
+        if(idx==numbers.length){
+            if(val==target)
+                answer++;
+            return;
+        }
+        dfs(val+numbers[idx],idx+1);
+        dfs(val-numbers[idx],idx+1);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        dfs(0,0);
+        return answer;
+    }
+}
