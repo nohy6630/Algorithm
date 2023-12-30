bb5dd9c0daf243d9df8f9c2b4e108f1f8baa1b9c
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2023-12-30 22:55:22 +0900

    [level 4] Title: 사칙연산, Time: 13.86 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    int[][] dpMin = new int[111][111], dpMax = new int[111][111];
+    int[] sign = new int[111];
+    
+    public int solution(String arr[]) {
+        int idx = 0;
+        for(String s : arr){
+            if(s.equals("-") || s.equals("+")){
+                if(s.equals("-"))
+                    sign[idx] = -1;
+                else
+                    sign[idx] = 1;
+                continue;
+            }
+            dpMax[idx][idx] = dpMin[idx][idx] = Integer.parseInt(s);
+            idx++;
+        }
+        for(int d=1; d<idx; d++){
+            for(int i=0; i<idx; i++){
+                int j = i + d;
+                if(j > idx)
+                    continue;
+                dpMax[i][j] = Integer.MIN_VALUE;
+                dpMin[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<j; k++){
+                    if(sign[k+1] == 1){
+                        dpMin[i][j] = Math.min(dpMin[i][j], dpMin[i][k] + sign[k+1] * dpMin[k+1][j]);
+                        dpMax[i][j] = Math.max(dpMax[i][j], dpMax[i][k] + sign[k+1] * dpMax[k+1][j]);
+                    }
+                    else{
+                        dpMin[i][j] = Math.min(dpMin[i][j], dpMin[i][k] + sign[k+1] * dpMax[k+1][j]);
+                        dpMax[i][j] = Math.max(dpMax[i][j], dpMax[i][k] + sign[k+1] * dpMin[k+1][j]);
+                    }                  
+                }
+            }
+        }
+        return dpMax[0][idx-1];
+    }
+}
