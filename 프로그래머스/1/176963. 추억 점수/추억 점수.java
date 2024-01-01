21fa859b337ffb64e6a7bffb6a6c6127c61eb7e3
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2024-01-01 20:21:26 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> score = new HashMap<>();
+        for(int i=0;i<name.length;i++)
+            score.put(name[i],yearning[i]);
+        for(int i=0;i<photo.length;i++){
+            for(String n : photo[i])
+                answer[i] += score.getOrDefault(n, 0);
+        }
+        return answer;
+    }
+}
