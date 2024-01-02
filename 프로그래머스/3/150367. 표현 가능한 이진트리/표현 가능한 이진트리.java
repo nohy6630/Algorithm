5a9aa94a68dd06851efc462439a90aa6b361d4e7
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2024-01-02 22:26:54 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 11.40 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,42 +1,6 @@
 class Solution {
-    int chkTree(int[] binary, int l, int r){
-        if(l>r)
-            return 0;
-        int m = (l+r)/2;
-        if(binary[m]==0)
-            return 0;
-        int ret = 1;
-        ret += chkTree(binary, l, m-1);
-        ret += chkTree(binary, m+1, r);
-        return ret;
-    }
-    
-    int chkMakable(long n){
-        int[] arr = new int[100];
-        int idx = 99;
-        long tmp = n;
-        while(tmp>0){
-            arr[idx] = (int)(tmp%2);
-            idx--;
-            tmp/=2;
-        }
-        int digit = (int)Math.pow(2,Math.ceil(Math.log(99-idx+1)/Math.log(2)))-1;
-        int[] binary = new int[digit];
-        int cnt = 0;
-        for(int i=0;i<digit;i++){
-            binary[i]=arr[99-digit+1+i];
-            if(binary[i]==1)
-                cnt++;
-        }
-        if(chkTree(binary,0,binary.length-1) == cnt)
-            return 1;
-        return 0;
-    }
-    
     public int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i=0;i<answer.length;i++)
-            answer[i]=chkMakable(numbers[i]);
+        int[] answer = {};
         return answer;
     }
 }
