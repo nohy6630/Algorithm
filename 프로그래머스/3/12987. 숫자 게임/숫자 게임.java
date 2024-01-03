b8a099805b4291eaa925765366652cf98d3f5f1b
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2024-01-03 17:54:00 +0900

    [level 3] Title: 숫자 게임, Time: 59.33 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int i = A.length-1, j = B.length-1;
+        while(i>=0 && j>=0){
+            if(B[j]>A[i]){
+                answer++;
+                j--;
+            }
+            i--;
+        }
+        return answer;
+    }
+}
