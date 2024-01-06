5492fc7c8bde3e2b381d5b1517d7f10549b49a1b
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2024-01-06 20:15:53 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,4 @@
+select b.title,r.board_id,r.reply_id,r.writer_id,r.contents,date_format(r.created_date,'%Y-%m-%d')
+    from used_goods_board b join used_goods_reply r on b.board_id = r.board_id
+    where year(b.created_date) = 2022 and month(b.created_date) = 10
+    order by r.created_date asc, b.title asc;
