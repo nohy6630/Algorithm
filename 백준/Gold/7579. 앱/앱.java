b0cb716fe86fad1d7df17168bef2c5f7f57453d3
Yeongjin Noh <129354455+nohy6630@users.noreply.github.com>
2024-02-23 15:35:36 +0900

    [Gold III] Title: 앱, Time: 184 ms, Memory: 24944 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,36 +1,36 @@
-import java.util.*;
-import java.util.stream.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    int N, M;
-    int[] m = new int[101], c = new int[101];
-    long[][] dp = new long[101][10001];
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
-    int ans = Integer.MAX_VALUE;
+    StringBuilder sb = new StringBuilder();
+    long[][] dp = new long[111][11111];
+    int n, m;
+    int[] size = new int[111], cost = new int[111];
 
     void solution() throws Exception {
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++)
-            m[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++)
+            size[i] = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++)
-            c[i] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N * 100; j++) {
-                if (c[i] > j)
-                    dp[i][j] = dp[i - 1][j];
+        for (int i = 1; i <= n; i++)
+            cost[i] = Integer.parseInt(st.nextToken());
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= 100 * n; j++) {
+                if (j >= cost[i])
+                    dp[i][j] = Math.max(dp[i - 1][j - cost[i]] + (long)size[i], dp[i - 1][j]);
                 else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - c[i]] + m[i]);
-                if (dp[i][j] >= M)
+                    dp[i][j] = dp[i - 1][j];
+                if (dp[i][j] >= m)
                     ans = Math.min(ans, j);
             }
         }
-        System.out.print(ans);
+        System.out.println(ans);
     }
 
     public static void main(String[] args) throws Exception {
